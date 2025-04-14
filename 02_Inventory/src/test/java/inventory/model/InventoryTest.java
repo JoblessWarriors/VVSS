a23832181892107c02556de2114235ad5225a3c1
Paula Haidamac <haidamacpaula@gmail.com>
2025-04-14 19:26:15 +0300

    Implemented tests

--- 02_Inventory/src/test/java/inventory/model/InventoryTest.java
+++ 02_Inventory/src/test/java/inventory/model/InventoryTest.java
@@ -1,9 +1,12 @@
 package inventory.model;
 
+import inventory.repository.InventoryRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,6 +16,17 @@ class InventoryTest {
     @BeforeEach
     void setUp() {
         inventory = new Inventory();
+    }
+
+    @AfterEach
+    void tearDown() {
+        inventory = null;
+    }
+
+    @Test
+    @DisplayName("F02a_TC01-WBT-Valid-Lookup Part")
+    void f02_tc01_lookupPart() {
+        //Arrange
         inventory.addPart(
                 new Part(
                         1,
@@ -37,36 +51,137 @@ class InventoryTest {
                         5,
                         1,
                         10));
-    }
-
-    @AfterEach
-    void tearDown() {
-        inventory = null;
-    }
-
-    @Test
-    @DisplayName("F02a_TC01-WBT-Valid-Lookup Part")
-    void f02_tc01_lookupPart() {
-        //Arrange
         var searchItem = "1part";
+        var expectedResult = inventory.getAllParts().get(0);
 
         //Act
-        var part = inventory.lookupPart(searchItem);
+        var actualResult = inventory.lookupPart(searchItem);
 
         //Assert
-        assertEquals(part, inventory.getAllParts().get(0));
+        assertEquals(actualResult, expectedResult);
     }
 
     @Test
-    @DisplayName("F02a_TC01-WBT-Valid-Lookup Part")
+    @DisplayName("F02a_TC02-WBT-Valid-Lookup Part")
     void f02_tc02_lookupPart() {
         //Arrange
-        var searchItem = "1part";
+        inventory.addPart(
+                new Part(
+                        1,
+                        "1part",
+                        10.0,
+                        5,
+                        1,
+                        10));
+        inventory.addPart(
+                new Part(
+                        2,
+                        "2",
+                        10.0,
+                        5,
+                        1,
+                        10));
+        inventory.addPart(
+                new Part(
+                        3,
+                        "3part",
+                        10.0,
+                        5,
+                        1,
+                        10));
+        var searchItem = "2";
+        var expectedResult = inventory.getAllParts().get(1);
 
         //Act
-        var part = inventory.lookupPart(searchItem);
+        var actualResult = inventory.lookupPart(searchItem);
 
         //Assert
-        assertEquals(part, inventory.getAllParts().get(0));
+        assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    @DisplayName("F02a_TC03-WBT-Valid-Lookup Part")
+    void f02_tc03_lookupPart() {
+        //Arrange
+        inventory.addPart(
+                new Part(
+                        1,
+                        "1part",
+                        10.0,
+                        5,
+                        1,
+                        10));
+        inventory.addPart(
+                new Part(
+                        2,
+                        "2",
+                        10.0,
+                        5,
+                        1,
+                        10));
+        inventory.addPart(
+                new Part(
+                        3,
+                        "3part",
+                        10.0,
+                        5,
+                        1,
+                        10));
+        var searchItem = "pa";
+        var expectedResult = inventory.getAllParts().get(2);
+
+        //Act
+        var actualResult = inventory.lookupPart(searchItem);
+
+        //Assert
+        assertEquals(actualResult, expectedResult);
+    }
+
+    @Test
+    @DisplayName("F02a_TC04-WBT-Not valid-Lookup Part")
+    void f02_tc04_lookupPart() {
+        //Arrange
+        inventory.addPart(
+                new Part(
+                        1,
+                        "1part",
+                        10.0,
+                        5,
+                        1,
+                        10));
+        inventory.addPart(
+                new Part(
+                        2,
+                        "2",
+                        10.0,
+                        5,
+                        1,
+                        10));
+        inventory.addPart(
+                new Part(
+                        3,
+                        "3part",
+                        10.0,
+                        5,
+                        1,
+                        10));
+        var searchItem = "aa";
+
+        //Act & Assert
+        assertThrows(NoSuchElementException.class, () ->
+                inventory.lookupPart(searchItem)
+        );
+    }
+
+    @Test
+    @DisplayName("F02a_TC05-WBT-Not valid-Lookup Part")
+    void f02_tc05_lookupPart() {
+        //Arrange
+        var searchItem = "qwfqew";
+
+        //Act & Assert
+        assertThrows(NoSuchElementException.class, () ->
+                inventory.lookupPart(searchItem)
+        );
     }
 }
